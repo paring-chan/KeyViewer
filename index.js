3dfd44079a4e504b8185f8396ff66e370d2c80d1
pikokr <parangee9706@gmail.com>
2020-11-11 21:28:13 +0900

    다만들었다아아

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron')
 const config = require('./config.json')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
-function createWindow () {
-    win = new BrowserWindow({ width: 800, height: 600, frame: false,
-    alwaysOnTop: Boolean(config.top), webPreferences: {
-        nodeIntegration: true
-        }
-         })
+function createWindow() {
+    win = new BrowserWindow({
+        width: 50 * config.keys.length, height: 50, frame: false,
+        alwaysOnTop: Boolean(config.top),
+        webPreferences: {
+            nodeIntegration: true
+        },
+        resizable: false
+    })
 
     win.loadFile('src/index.html')
 
