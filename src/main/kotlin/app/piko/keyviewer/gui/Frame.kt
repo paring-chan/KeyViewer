db6db070c2ec11bd75f54acad457e4a41352aacd
pikokr <parangee9706@gmail.com>
2020-11-13 15:53:29 +0900

    ui

--- src/main/kotlin/app/piko/keyviewer/gui/Frame.kt
+++ src/main/kotlin/app/piko/keyviewer/gui/Frame.kt
@@ -1,11 +1,23 @@
 package app.piko.keyviewer.gui
 
+import java.awt.GridLayout
 import javax.swing.JFrame
+import javax.swing.JPanel
 
 class Frame : JFrame() {
+    private val panel = JPanel()
+    private val layout = GridLayout(1,4)
+
     init {
+        panel.layout = layout
+        contentPane = panel
         isUndecorated = true
-        setBounds(100,100,200,50)
+        setSize(200,50)
         isAlwaysOnTop = true
+        val buttons = Array(4) {
+            val btn = Key("$it")
+            btn
+        }
+        buttons.forEach { panel.add(it) }
     }
 }
---
+++ src/main/kotlin/app/piko/keyviewer/gui/Key.kt
@@ -0,0 +1,19 @@
+package app.piko.keyviewer.gui
+
+import java.awt.Color
+import javax.swing.JButton
+import javax.swing.plaf.metal.MetalButtonUI
+
+class Key(label: String) : JButton(label) {
+    init {
+        isBorderPainted = false
+        isFocusPainted = false
+        isEnabled = false
+        background = Color.BLACK
+        setUI(object : MetalButtonUI() {
+            override fun getDisabledTextColor(): Color {
+                return Color.WHITE
+            }
+        })
+    }
+}
